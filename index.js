4eaa6c8dae877379be65cbf3788490e3b6b0dba3
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-21 07:24:55 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+function changeText (){
+    const newText = document.getElementById('text').innerHTML = 'This is really cool!';
+    return newText;
+};
+document.addEventListener('DOMContentLoaded',changeText);
